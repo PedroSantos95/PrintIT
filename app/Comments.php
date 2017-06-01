d37590249fb265fb397fcc38e9ff067b41437b56
PedroSantos95 <pedromverasantos@gmail.com>
2017-06-01 21:09:16 +0100

    All Relations

--- app/Comments.php
+++ app/Comments.php
@@ -17,4 +17,23 @@ class Comments extends Model
     	return $this->belongsTo(RequestPrint::class);
     }
 
+    public function request()
+    {
+    	return $this->belongsTo('App\RequestPrint');
+    }
+
+    public function parent()
+    {
+    	return $this->belongsTo('App\Comments', 'parent_id');
+    }
+
+    public function user()
+    {
+    	return $this->belongsTo('App\User');
+    }
+
+    public function children ()
+    {
+    	return $this->hasMany('App\Comments', 'parent_id');
+    }
 }
--- app/Department.php
+++ app/Department.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Department extends Model
 {
-    //
+    
+    public function users()
+    {
+    	return $this->hasMany('App\User', 'department_id');
+    }
+    
 }
