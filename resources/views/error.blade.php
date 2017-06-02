c1aaa31bbc87572806b5f4bd7be41371abf9a303
PedroSantos95 <pedromverasantos@gmail.com>
2017-06-02 20:07:07 +0100

    Error View

---
+++ resources/views/error.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.header')
+
+@yield('content')
+
+<br><br>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 ">
+         <div class="panel panel-default">
+                <div class="panel-heading"><b>Error!</b></div>
+                <div class="panel-body">
+        You dont have premissions to see this page! Please contact your support for adicional information.
+        </div>
+        </div>
+        </div>
+         </div>
+        </div>
+        </div>
+<br>
+@extends('layouts.footer')
