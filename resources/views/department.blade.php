0078c338810a6cdeb19510b8483a47f3ceed6090
PedroSantos95 <pedromverasantos@gmail.com>
2017-06-03 02:03:05 +0100

    Search on Tables

--- resources/views/department.blade.php
+++ resources/views/department.blade.php
@@ -1,6 +1,36 @@
 @extends('layouts.header')
 
 @yield('content')
+<script type="text/javascript">
+function doSearch() {
+    var searchText = document.getElementById('searchTerm').value;
+    var targetTable = document.getElementById('listUsers');
+    var targetTableColCount;
+            
+    //Loop through table rows
+    for (var rowIndex = 0; rowIndex < targetTable.rows.length; rowIndex++) {
+        var rowData = '';
+
+        //Get column count from header row
+        if (rowIndex == 0) {
+           targetTableColCount = targetTable.rows.item(rowIndex).cells.length;
+           continue; //do not execute further code for header row.
+        }
+                
+        //Process data rows. (rowIndex >= 1)
+        for (var colIndex = 0; colIndex < targetTableColCount; colIndex++) {
+            rowData += targetTable.rows.item(rowIndex).cells.item(colIndex).textContent;
+        }
+
+        //If search term is not found in row data
+        //then hide the row, else show
+        if (rowData.indexOf(searchText) == -1)
+            targetTable.rows.item(rowIndex).style.display = 'none';
+        else
+            targetTable.rows.item(rowIndex).style.display = 'table-row';
+    }
+}
+</script>
 <br><br><br>
 
 <div class="container">
@@ -11,6 +41,11 @@
 				<div class="panel-body">
 					<div align="center">
 						<table id="listUsers" class="table table-hover sortable" id="users-admin" style="widows: 80%;">
+							<div class="row"> 
+			<div class="col-md-1 col-md-offset-9" style="padding-left: 50px">
+				<input placeholder="    Search here:" type="text" id="searchTerm" class="search_box" onkeyup="doSearch()" />
+			</div>
+	</div>
 							<thead class="thead-inverse">
 								<tr>
 									<th style="text-align: center" class="col-xs-1">Department ID</th>
--- resources/views/printers.blade.php
+++ resources/views/printers.blade.php
@@ -1,10 +1,45 @@
 @extends('layouts.header')
 
 @yield('content')
+<script type="text/javascript">
+function doSearch() {
+    var searchText = document.getElementById('searchTerm').value;
+    var targetTable = document.getElementById('listUsers');
+    var targetTableColCount;
+            
+    //Loop through table rows
+    for (var rowIndex = 0; rowIndex < targetTable.rows.length; rowIndex++) {
+        var rowData = '';
+
+        //Get column count from header row
+        if (rowIndex == 0) {
+           targetTableColCount = targetTable.rows.item(rowIndex).cells.length;
+           continue; //do not execute further code for header row.
+        }
+                
+        //Process data rows. (rowIndex >= 1)
+        for (var colIndex = 0; colIndex < targetTableColCount; colIndex++) {
+            rowData += targetTable.rows.item(rowIndex).cells.item(colIndex).textContent;
+        }
+
+        //If search term is not found in row data
+        //then hide the row, else show
+        if (rowData.indexOf(searchText) == -1)
+            targetTable.rows.item(rowIndex).style.display = 'none';
+        else
+            targetTable.rows.item(rowIndex).style.display = 'table-row';
+    }
+}
+</script>
 <br>
 @if(Auth::check())
 <div align="center">
 	<table id="listPrinters" class=" table table-hover sortable" id="users-admin" style="width:80%;">
+		<div class="row"> 
+			<div class="col-md-1 col-md-offset-9" style="padding-left: 50px">
+				<input placeholder="    Search here:" type="text" id="searchTerm" class="search_box" onkeyup="doSearch()" />
+			</div>
+	</div>
 		<thead class="thead-inverse">
 			<tr>
 				<th style="text-align: center" class="col-xs-1">Name</th>
